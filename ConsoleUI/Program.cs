08181c2227f609b271bfa42984e9684de3e2b4d3
Cihan Barış Alkaya <Cihan Bar Alkaya@DESKTOP-EP6LVSO>
2022-12-06 16:58:51 +0300

    Adding Dtos

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,7 +8,8 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CategoryTest();
+            //CategoryTest();
+            ProductTest();
         }
 
         private static void CategoryTest()
@@ -23,9 +24,9 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+"/"+product.CategoryName);
             }
         }
     }
